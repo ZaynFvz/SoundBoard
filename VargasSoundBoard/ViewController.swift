4d877457219ea275597c1c8b2533cda9c0deae1b
ZaynFvz <gonzalo.vargas@tecsup.edu.pe>
2023-05-24 08:34:38 -0500

    Initial Commit

---
+++ VargasSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VargasSoundBoard
+//
+//  Created by Gonzalo Vargas on 24/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
